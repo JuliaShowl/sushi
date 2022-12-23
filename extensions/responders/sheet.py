b52417b51c628ec2230a89a46217acd49cd09bbb
Julia Showl <juliashowl@gwu.edu>
2022-12-23 16:14:47 -0500

    support adding to database from discord

--- extensions/responders/sheet.py
+++ extensions/responders/sheet.py
@@ -62,5 +62,62 @@ async def souris_plant(ctx):
     except HttpError as err:
         print(err)
 
+@plugin.command
+# @lightbulb.add_checks(lightbulb.has_roles(<roleID>,mode=any)) Uncomment to limit
+@lightbulb.option("image","Image to be added", type=str, required=True)
+@lightbulb.option("responder", "Responder to be added to", type=str, required=True, choices=["sushi", "egg", "souris_plant"])
+@lightbulb.command("add", "Add picture to responder", auto_defer=True, pass_options=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx: lightbulb.Context, responder: str, image: str):
+    try:
+        service = build('sheets', 'v4', credentials=credentials)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+
+        # How the input data should be interpreted.
+        value_input_option = 'USER_ENTERED'
+    except HttpError as err:
+        print(err)
+
+    if(responder == "sushi"):
+        try:
+            rng = 'Sheet1!A:A'
+            value_range_body = {
+                "majorDimension": "COLUMNS",
+                'values': [[image]]
+            }
+
+            sheet.values().append(spreadsheetId=SUSHI, range=rng, valueInputOption=value_input_option, body=value_range_body).execute()
+        except HttpError as err:
+            print(err)
+    elif(responder == "egg"):
+        try:
+            rng = 'Sheet1!B:B'
+            value_range_body = {
+                "majorDimension": "COLUMNS",
+            'values': [[image]]
+            }
+
+            sheet.values().append(spreadsheetId=SUSHI, range=rng, valueInputOption=value_input_option, body=value_range_body).execute()
+        except HttpError as err:
+            print(err)
+    elif(responder == "souris_plant"):
+        try:
+            rng = 'Sheet1!C:C'
+            value_range_body = {
+                "majorDimension": "COLUMNS",
+            'values': [[image]]
+            }
+
+            sheet.values().append(spreadsheetId=SUSHI, range=rng, valueInputOption=value_input_option, body=value_range_body).execute()
+        except HttpError as err:
+            print(err)
+    else:
+        await ctx.respond("Responder not recgonized")
+        return
+
+    await ctx.respond("Added to bot!")
+
 def load(bot):
     bot.add_plugin(plugin)
--- extensions/utilities/help.py
+++ extensions/utilities/help.py
@@ -10,11 +10,12 @@ async def help(ctx):
     sushi = "`/sushi` - Returns a picture of Sushi, Mia's hamster\n"
     egg = "`/egg` - Returns a picture of Egg, Souris' cat\n"
     plant = "`/souris_plant` - Returns a picture of Souris' bonsai or some random Reddit plant\n"
+    add = "`/add <responder> <image>` - Adds link to image database.\n"
     zim = "`/zimzalabim` - Are you ready for this?\n"
     pan = "`/pantomime` - Like a pantomime\n"
     math = "`/solve <equation>` - Solves equations. Supports python's math library\n"
     yoink = "`/yoihnk <emote>` - Grabs custom emote\n"
-    resp = f"{sushi}{egg}{plant}{zim}{pan}{math}{yoink}"
+    resp = f"**Media Responders**\n{sushi}{egg}{plant}{zim}{pan}{add}\n**Misc Responders**\n{math}\n**Utilities**\n{yoink}"
     embed = hikari.Embed(title="SushiBot Help",description=resp, color='b0ffe3')
     await ctx.respond(embed=embed)
 def load(bot):
