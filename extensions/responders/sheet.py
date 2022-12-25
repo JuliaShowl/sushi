6e074eca2c7a29225d51e0c6b7ca9b6bcd71da24
Julia Showl <juliashowl@gwu.edu>
2022-12-25 14:43:07 -0500

    redo sheet responders

--- extensions/responders/sheet.py
+++ extensions/responders/sheet.py
@@ -39,7 +39,7 @@ async def egg(ctx):
         # Call the Sheets API
         sheet = service.spreadsheets()
         result = sheet.values().get(spreadsheetId=SUSHI,
-                                    range="Sheet1!B:B").execute()
+                                    range="Sheet2!A:A").execute()
         values = result.get('values', [])
         i = random.randrange(0,len(values))
         await ctx.respond(values[i][0])
@@ -55,20 +55,20 @@ async def souris_plant(ctx):
         # Call the Sheets API
         sheet = service.spreadsheets()
         result = sheet.values().get(spreadsheetId=SUSHI,
-                                    range="Sheet1!C:C").execute()
+                                    range="Sheet3!A:A").execute()
         values = result.get('values', [])
         i = random.randrange(0,len(values))
         await ctx.respond(values[i][0])
     except HttpError as err:
         print(err)
-
+    
 @plugin.command
-# @lightbulb.add_checks(lightbulb.has_roles(<roleID>,mode=any)) Uncomment to limit
-@lightbulb.option("image","Image to be added", type=str, required=True)
+@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES))
+@lightbulb.option("media","Media to be added", type=str, required=True)
 @lightbulb.option("responder", "Responder to be added to", type=str, required=True, choices=["sushi", "egg", "souris_plant"])
-@lightbulb.command("add", "Add picture to responder", auto_defer=True, pass_options=True)
+@lightbulb.command("add", "Add media to database", auto_defer=True, pass_options=True)
 @lightbulb.implements(lightbulb.SlashCommand)
-async def add(ctx: lightbulb.Context, responder: str, image: str):
+async def add(ctx: lightbulb.Context, responder: str, media: str):
     try:
         service = build('sheets', 'v4', credentials=credentials)
 
@@ -77,47 +77,116 @@ async def add(ctx: lightbulb.Context, responder: str, image: str):
 
         # How the input data should be interpreted.
         value_input_option = 'USER_ENTERED'
+        value_range_body = {
+                "majorDimension": "COLUMNS",
+                'values': [[media]]
+        }
     except HttpError as err:
         print(err)
 
     if(responder == "sushi"):
         try:
-            rng = 'Sheet1!A:A'
-            value_range_body = {
-                "majorDimension": "COLUMNS",
-                'values': [[image]]
-            }
-
-            sheet.values().append(spreadsheetId=SUSHI, range=rng, valueInputOption=value_input_option, body=value_range_body).execute()
+            sheet.values().append(spreadsheetId=SUSHI, range="Sheet1!A:A", valueInputOption=value_input_option, body=value_range_body).execute()
         except HttpError as err:
             print(err)
     elif(responder == "egg"):
         try:
-            rng = 'Sheet1!B:B'
-            value_range_body = {
-                "majorDimension": "COLUMNS",
-            'values': [[image]]
-            }
-
-            sheet.values().append(spreadsheetId=SUSHI, range=rng, valueInputOption=value_input_option, body=value_range_body).execute()
+            sheet.values().append(spreadsheetId=SUSHI, range="Sheet2!A:A", valueInputOption=value_input_option, body=value_range_body).execute()
         except HttpError as err:
             print(err)
     elif(responder == "souris_plant"):
         try:
-            rng = 'Sheet1!C:C'
-            value_range_body = {
-                "majorDimension": "COLUMNS",
-            'values': [[image]]
-            }
-
-            sheet.values().append(spreadsheetId=SUSHI, range=rng, valueInputOption=value_input_option, body=value_range_body).execute()
+            sheet.values().append(spreadsheetId=SUSHI, range="Sheet3!A:A", valueInputOption=value_input_option, body=value_range_body).execute()
         except HttpError as err:
             print(err)
     else:
         await ctx.respond("Responder not recgonized")
         return
 
-    await ctx.respond(f"Added {image} to `{responder}`!")
+    await ctx.respond(f"Added {media} to `{responder}`!")
+
+@plugin.command
+@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES))
+@lightbulb.option("media","Media to be removed", type=str, required=True)
+@lightbulb.command("remove", "Remove media from database", auto_defer=True, pass_options=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx: lightbulb.Context, media: str):
+    try:
+        service = build('sheets', 'v4', credentials=credentials)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+
+        value_range_body = {}
+        result = sheet.values().get(spreadsheetId=SUSHI,
+                                    range="Sheet1!A:A").execute()
+        values = result.get('values', [])
+
+        for i in range(len(values)):
+            if(values[i][0] == media):
+                request_body = {
+                    "requests":[{
+                            "deleteDimension": {
+                                "range": {
+                                "sheetId": '0',
+                                "dimension": "ROWS",
+                                "startIndex": i,
+                                "endIndex": i+1
+                                }
+                            }
+                        }]
+                }  
+                sheet.batchUpdate(spreadsheetId=SUSHI,body=request_body).execute()
+                await ctx.respond(f"Removed {media} to from database!")
+                return
+
+        result = sheet.values().get(spreadsheetId=SUSHI,
+                                    range="Sheet2!A:A").execute()
+        values = result.get('values', [])
+
+        for i in range(len(values)):
+            if(values[i][0] == media):
+                request_body = {
+                    "requests":[{
+                            "deleteDimension": {
+                                "range": {
+                                "sheetId": '1802442433',
+                                "dimension": "ROWS",
+                                "startIndex": i,
+                                "endIndex": i+1
+                                }
+                            }
+                        }]
+                }  
+                sheet.batchUpdate(spreadsheetId=SUSHI,body=request_body).execute()
+                await ctx.respond(f"Removed {media} to from database!")
+                return
+        
+        result = sheet.values().get(spreadsheetId=SUSHI,
+                                    range="Sheet3!A:A").execute()
+        values = result.get('values', [])
+
+        for i in range(len(values)):
+            if(values[i][0] == media):
+                request_body = {
+                    "requests":[{
+                            "deleteDimension": {
+                                "range": {
+                                "sheetId": '2057942648',
+                                "dimension": "ROWS",
+                                "startIndex": i,
+                                "endIndex": i+1
+                                }
+                            }
+                        }]
+                }  
+                sheet.batchUpdate(spreadsheetId=SUSHI,body=request_body).execute()
+                await ctx.respond(f"Removed {media} to from database!")
+                return
+    except HttpError as err:
+        print(err)
+
+    await ctx.respond(f"Unable to find media")
 
 def load(bot):
     bot.add_plugin(plugin)
