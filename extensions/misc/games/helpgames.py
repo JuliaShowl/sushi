3f5b1f4eff49eb3aa91ab49ed6ec49b21df26cea
Julia Showl <juliashowl@gwu.edu>
2023-01-16 14:08:01 -0500

    fix formatting

--- extensions/misc/games/helpgames.py
+++ extensions/misc/games/helpgames.py
@@ -10,8 +10,8 @@ async def help(ctx):
     fq = "`/fq <count|optional> <options|optional>` - Guess the flag\n"
     cq = "`/cq <count|optional> <options|optional>` - Guess the capital\n"
     trivia = "`/trivia <count|optional> <category|optional> <difficulty|optional>` - Get trivia questions\n"
-    vs = "`/vs <type> <count|optional> <category|optional> <difficulty|optional> <options|optional> - Play the quizzes against friends.\n"
-    rps = "`/rps <players|optional> <user|optional> - Play rock, paper, scissors against Sushi or a friend!\n"
+    vs = "`/vs <type> <count|optional> <category|optional> <difficulty|optional> <options|optional>` - Play the quizzes against friends.\n"
+    rps = "`/rps <players|optional> <user|optional>` - Play rock, paper, scissors against Sushi or a friend!\n"
     resp = f"**Games**\n{fq}{cq}{trivia}{vs}{rps}"
     embed = hikari.Embed(title="SushiBot Help",description=resp, color='b0ffe3')
     await ctx.respond(embed=embed)
