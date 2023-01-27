19028c36fe6e68cb121189b334d5f352845b7b2a
Julia Showl <juliashowl@gwu.edu>
2023-01-26 19:09:48 -0500

    add error handling for papago

--- extensions/utilities/translate.py
+++ extensions/utilities/translate.py
@@ -27,37 +27,37 @@ plugin.add_checks(
 @lightbulb.implements(lightbulb.SlashCommand)
 async def translate(ctx: lightbulb.Context, query: str, source: str, target: str):
     target = target or 'en'
+    source = source or 'auto'
     if source == 'kr': source = 'ko'
     if source == 'jp': source = 'ja'
     if target == 'kr': target = 'ko'
     if target == 'jp': target = 'ja'
 
     if source == 'ko' or source == 'korean' or target == 'ko' or target == 'korean' or source == 'ja' or source == "japanese" or target == 'ja' or target == "japanese":
-        client, secret = random.choice(list(dict.items()))
         payload = {"source": source, "target": target, "text": query}
         headers = {
             "X-Naver-Client-Id": client,
             "X-Naver-Client-Secret": secret,
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         }
-        response = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=payload)
-        res_body = json.loads(response.text)
-        msg = res_body.get("message")
-        result = msg.get("result", None)
-        if not result:
-            translator = GoogleTranslator(source=source, target=target)
-            result = translator.translate(query)
+        try: 
+            response = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=payload)
+            res_body = json.loads(response.text)
+            msg = res_body.get("message")
+            result = msg.get("result", None)
+            if not result:
+                translator = GoogleTranslator(source=source, target=target)
+                result = translator.translate(query)
+                await ctx.respond(f'{query} -> {result}')
+                return
+                
+            result = result.get("translatedText")
             await ctx.respond(f'{query} -> {result}')
             return
-            
-        result = result.get("translatedText")
-        await ctx.respond(f'{query} -> {result}')
-        return
-    elif source is not None:
-        translator = GoogleTranslator(source=source, target=target)
+        except:
+            pass
     else:
         translator = GoogleTranslator(source='auto', target=target)
-
     try:
         result = translator.translate(query)
         
@@ -75,8 +75,6 @@ async def langauges(ctx: lightbulb.context):
         langs = langs + "`(" + a + ")` - " + l + "\n"
     embed = hikari.Embed(title="Supported Languages", description=langs)
     await ctx.respond(embed=embed)
-
-
 
 def load(bot):
     bot.add_plugin(plugin)
