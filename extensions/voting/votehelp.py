c92423eb2d77d3fada8b2f310af5faebc13e763c
Julia Showl <juliashowl@gwu.edu>
2023-01-29 14:56:02 -0500

    update help

--- extensions/voting/votehelp.py
+++ extensions/voting/votehelp.py
@@ -11,10 +11,9 @@ plugin.add_checks(
 @lightbulb.implements(lightbulb.SlashCommand)
 async def votehelp(ctx):
     va = "`/va <count|optional>` - Gets voting account info.\n"
-    ga = "`/ga` - Generates account information for sign up.\n"
     votecount = "`/votecount` - Gets account totals.\n"
     reset = "`/reset` - Resets voting accounts. *Limited to bot owner*\n"
-    resp = f"{va}{ga}{votecount}{reset}"
+    resp = f"{va}{votecount}{reset}"
     embed = hikari.Embed(title="SushiBot Voting Help",description=resp, color='b0ffe3')
     await ctx.respond(embed=embed)
 def load(bot):
