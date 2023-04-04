739946df62f21dbb9b9ed4d0a0f8ceac5360ac23
Julia Showl <juliashowl@gwu.edu>
2023-04-04 17:15:58 -0400

    rewrite help

--- extensions/utilities/help.py
+++ extensions/utilities/help.py
@@ -9,13 +9,16 @@ plugin = lightbulb.Plugin('help')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def help(ctx):
     help = []
+    embed = hikari.Embed(title="SushiBot Help", color='b0ffe3')
     sushi = "`/sushi` - Returns a picture of Sushi, Mia's hamster\n"
     egg = "`/egg` - Returns a picture of Egg, Souris' cat\n"
     plant = "`/souris_plant` - Returns a picture of Souris' bonsai or some random Reddit plant\n"
-    munchie = "`/munchie` - Returns a picture of Munchie.\n"
+    munchie = "`/munchie` - Returns a picture of Munchie and Friends.\n"
+    bread = "`/bread` - Returns a picture of bread.\n"
     pic = "`/pic <query> <quantity|optional>` - Get pictures!\n"
     add = "`/add <responder> <media>` - Adds media to database. Supports embeddable links, discord messages, and tweets.\n"
-    remove = "`/remove <media>` - Removes media from database.\n"
+    remove = "`/remove <media> <responder|optional>` - Removes media from database.\n"
+    moon = "`/moonlightsunrise` - Baby come be my starlight\n"
     zim = "`/zimzalabim` - Are you ready for this?\n"
     pan = "`/pantomime` - Like a pantomime\n"
     math = "`/solve <equation>` - Solves equations. Supports python's math library\n"
@@ -29,18 +32,21 @@ async def help(ctx):
     languages = "`/lnguages` - Get a list of supported languages.\n"
     twt = "`/twt <tweet>` - Get media from a tweet.\n"
     search = "`/search <query>` - Search the web.\n"
-    whois = "`/whois <user|optional>` - Get information on a user.\n"
+    whois = "`/whois <user|optional>` - Get information about a user.\n"
     serverinfo = "`/serverinfo` - Get information about the server.\n"
-    helpresp = f"**Media Responders**\n{sushi}{egg}{munchie}{plant}{zim}{pan}{pic}{add}{remove}\n**Misc Responders**\n{math}{meme}{dadjoke}\n**Utilities**\n{translate}{languages}{search}{twt}{yoink}{yt}{yt_stats}{avatar}{whois}{serverinfo}"
-    help.append(hikari.Embed(title="SushiBot Help",description=helpresp, color='b0ffe3'))
+    embed.add_field("Media Responders", value=f"{sushi}{egg}{munchie}{plant}{bread}{moon}{zim}{pan}{pic}{add}{remove}")
+    embed.add_field("Misc Responders", value=f"{math}{meme}{dadjoke}")
+    embed.add_field("Utilities", value=f"{translate}{languages}{search}{twt}{yoink}{yt}{yt_stats}{avatar}{whois}{serverinfo}")
+    help.append(embed)
 
+    embed = hikari.Embed(title="SushiBot Help", color='b0ffe3')
     fq = "`/fq <count|optional> <options|optional>` - Guess the flag\n"
     cq = "`/cq <count|optional> <options|optional>` - Guess the capital\n"
     trivia = "`/trivia <count|optional> <category|optional> <difficulty|optional>` - Get trivia questions\n"
     vs = "`/vs <type> <count|optional> <category|optional> <difficulty|optional> <options|optional>` - Play the quizzes against friends.\n"
     rps = "`/rps <players|optional> <user|optional>` - Play rock, paper, scissors against Sushi or a friend!\n"
-    gameresp = f"**Games**\n{fq}{cq}{trivia}{vs}{rps}"
-    help.append(hikari.Embed(title="SushiBot Help",description=gameresp, color='b0ffe3'))
+    embed.add_field("Games", value=f"{fq}{cq}{trivia}{vs}{rps}")
+    help.append(embed)
 
     buttons = [nav.FirstButton(), nav.PrevButton(), nav.StopButton(), nav.NextButton(), nav.LastButton()]
     navigator = nav.NavigatorView(pages=help, buttons=buttons)
