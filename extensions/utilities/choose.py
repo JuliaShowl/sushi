bf2729ffd7e72fab093f0fe0961571727ba479a5
Julia Showl <juliashowl@gwu.edu>
2023-11-28 16:06:30 -0500

    update type in choose

--- extensions/utilities/choose.py
+++ extensions/utilities/choose.py
@@ -12,7 +12,7 @@ plugin.add_checks(
 @lightbulb.option('opt1', "First option", type=str, required=True)
 @lightbulb.command('choose','Choose between two options', pass_options=True)
 @lightbulb.implements(lightbulb.SlashCommand)
-async def choose(ctx: lightbulb.Context, opt1: str, opt2: int):
+async def choose(ctx: lightbulb.Context, opt1: str, opt2: str):
     choice = random.sample([opt1,opt2],1)
     await ctx.respond(f"I choose **{choice[0]}**")
 
